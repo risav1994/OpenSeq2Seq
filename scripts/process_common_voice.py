121bcfca5c701cede3a64283fd4fcbcd7f6a92fb
risav <risav.saingar@vmock.com>
2020-06-09 15:32:26 +0530

    data processing

--- scripts/process_common_voice.py
+++ scripts/process_common_voice.py
@@ -44,7 +44,6 @@ def main(_):
                     yt, index = librosa.effects.trim(y, top_db=10)
                     yt = y[max(index[0] - 40000, 0): min(index[1] + 40000, len(y))]
                     soundfile.write(wav_file, yt, sr)
-                    bar.update(1)
                 wav_filesize = os.path.getsize(wav_file)
                 data.append((os.path.abspath(wav_file), wav_filesize, transcript))
             except sox.core.SoxError:
