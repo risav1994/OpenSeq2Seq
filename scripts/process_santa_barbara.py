2909b6189c3b67cec5a1f2430ebfea8e508b1875
risav <risav.saingar@vmock.com>
2020-06-09 18:09:27 +0530

    data processing

---
+++ scripts/process_santa_barbara.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+import librosa
+import pandas as pd
+import os
+import unicodedata
+import soundfile
+import logging
+from tqdm import tqdm
+from glob import glob
+from sklearn.model_selection import train_test_split
+
+logging.basicConfig(level=logging.NOTSET)
+FLAGS = tf.compat.v1.app.flags.FLAGS
+
+
+def main(_):
+    source_dir = FLAGS.source_dir
+    transcripts = glob(source_dir + "/transcripts/TRN/*.trn")
+    df = pd.read_csv(transcripts[0], sep="\t")
+    print(df)
+
+
+if __name__ == "__main__":
+    tf.compat.v1.app.flags.DEFINE_string("source_dir", "source dir", "source directory")
+    tf.compat.v1.app.flags.DEFINE_float("test_size", 0.02, "test size")
+    tf.compat.v1.app.flags.DEFINE_integer("random_state", 1254, "random state")
+    tf.compat.v1.app.run()
