e3fa0c924cd6c0b0f025f35f24a332af6a358e78
risav <risav.saingar@vmock.com>
2020-06-10 10:03:22 +0530

    data processing

--- scripts/process_santa_barbara.py
+++ scripts/process_santa_barbara.py
@@ -13,7 +13,7 @@ from sklearn.model_selection import train_test_split
 logging.basicConfig(level=logging.NOTSET)
 FLAGS = tf.compat.v1.app.flags.FLAGS
 
-patterns = [r'\.', r'\(H\)', r'\(TSK\)', r'<YWN', r'?<=\s=+\b', r'\b=+']
+patterns = [r'\.+', r'\(H\)+', r'\(TSK\)+', r'<YWN+', r'?<=\s=+\b', r'\b=+']
 
 
 def main(_):
@@ -24,7 +24,7 @@ def main(_):
     for i in df.index:
         curr_transcript = df[columns[-1]][i]
         print(r'(' + "|".join(patterns) + r')+')
-        curr_transcript = re.sub(r'(' + "|".join(patterns) + r')+', '', curr_transcript)
+        curr_transcript = re.sub(r'(' + "|".join(patterns) + r')', '', curr_transcript)
         # curr_transcript = re.sub(r'(?<=\s=+\b|\b=+)')
         print(f"Regex Trans: {curr_transcript}, Orig Trans: {df[columns[-1]][i]}")
     print(df)
