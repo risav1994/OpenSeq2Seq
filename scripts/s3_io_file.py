a0ea295a5dbedd9fd229ec736c951490e6f6546c
risav <risav.saingar@vmock.com>
2020-06-23 12:32:35 +0530

    s3-stream

--- scripts/s3_io_file.py
+++ scripts/s3_io_file.py
@@ -10,11 +10,13 @@ class S3IOFile(object):
         self.s3_client = self.session.client("s3", config=Config(signature_version='s3v4', s3={"use_accelerate_endpoint": True}))
         self.s3_path = s3_path
         self.bucket = bucket
+        response = self.s3_client.head_object(Bucket=self.bucket, Key=self.s3_path)
+        self.content_size = response['ContentLength']
 
     def seek(self):
         pass
 
     def read(self, buf_size):
-        tar_obj = self.s3_client.get_object(Bucket=self.bucket, Key=self.s3_path)
+        tar_obj = self.s3_client.get_object(Bucket=self.bucket, Key=self.s3_path, Range="bytes=0-" + str(buf_size) + "/" + str(self.content_size))
         print(buf_size)
-        return tar_obj["Body"]
+        return tar_obj["Body"].read()
